542f2c7509629a4f23ca4f5cd804e66e08d3678d
Vaibhav <iosDeveloper1906@gmail.com>
2025-05-31 18:55:53 +0530

    Initial Commit

---
+++ To-DoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-DoList
+//
+//  Created by Vaibhav Gawde on 31/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-DoListTests/To_DoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoListTests.swift
+//  To-DoListTests
+//
+//  Created by Vaibhav Gawde on 31/05/25.
+//
+
+import Testing
+@testable import To_DoList
+
+struct To_DoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
